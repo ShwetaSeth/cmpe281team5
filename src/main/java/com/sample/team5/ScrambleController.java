cc65d36fd6244ec793edfc4d0218a45eafd08233
Shweta <shwetadseth@gmail.com>
2014-04-25 11:57:46 -0700

    fixing scramblewords

--- src/main/java/com/sample/team5/ScrambleController.java
+++ src/main/java/com/sample/team5/ScrambleController.java
@@ -1,5 +1,11 @@
 package com.sample.team5;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
@@ -32,16 +38,98 @@ public class ScrambleController {
 	ApplicationContext appContext = AppContext.getApplicationContext();
 	
 	@RequestMapping(value = "scramble", method = RequestMethod.GET)
-	public String scramble(Model model) {		
+	public String scramble(Model model) throws SQLException {		
 		ScrambleController con = (ScrambleController)appContext.getBean("scrambleController");		
-		con.getJSP();		
+		ScrambleDAO scrambleDAO = (ScrambleDAO)appContext.getBean("scrambleDAOImpl");
+		con.createScrambleTable();
+		con.createScrambleWords();
+		
+		scrambleDAO.setGame();	
 		model.addAttribute("score", "0");
 		return "scramble";
 	}
 	
-	public void getJSP(){
+	
+	public void createScrambleTable() throws SQLException{
+		Connection conn;
+
+		conn = dataSource.getConnection();
 		
-	}
+		DatabaseMetaData meta = conn.getMetaData();
+		ResultSet res = meta.getTables(null, null, "scramble", null);
+		if(res.next()){
+			System.out.println("Table 'scramble' already exists.");
+			logger.info("Table 'scramble' already exists.");
+		}
+		else{
+			Statement stmt = conn.createStatement();
+			
+			String query = "CREATE TABLE scramble (username VARCHAR(15) NOT NULL, prevScore INT NULL, "+
+					"currScore INT NULL, lastWord CHAR(15) NULL, PRIMARY KEY (username), CONSTRAINT username " +
+					"FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE ON UPDATE CASCADE)";
+			
+			stmt.executeUpdate(query);
+			logger.info("Table 'scramble' created.");
+			System.out.println("Table 'scramble' created.");
+			
+			query = "CREATE TABLE IF NOT EXISTS scramblewords ( Word CHAR(15) NOT NULL,PRIMARY KEY (word))";
+			stmt.executeUpdate(query);
+			logger.info("Table 'scramblewords' created.");
+			System.out.println("Table 'scramblewords' created.");
+			
+		}
+
+		Statement stmt = conn.createStatement();
+		String query = "INSERT IGNORE INTO scramblewords (word) VALUES ('riak')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('amazon')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('aws')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('dynamo')";
+		stmt.executeUpdate(query);
+
+		logger.info("Table scrablewords inserted with values");
+		System.out.println("Table scramblewords inserted with values");
+				
+		conn.close();			
+	}	
+	
+	public void createScrambleWords() throws SQLException{
+		Connection conn;
+
+		conn = dataSource.getConnection();
+		
+		DatabaseMetaData meta = conn.getMetaData();
+		ResultSet res = meta.getTables(null, null, "scramblewords", null);
+		if(res.next()){
+			System.out.println("Table 'scramble' already exists.");
+			logger.info("Table 'scramble' already exists.");
+		}
+		else{
+			Statement stmt = conn.createStatement();
+			String query = "CREATE TABLE IF NOT EXISTS scramblewords ( Word CHAR(15) NOT NULL,guess INT NULL DEFAULT 0,PRIMARY KEY (word))";
+			stmt.executeUpdate(query);
+			logger.info("Table 'scramblewords' created.");
+			System.out.println("Table 'scramblewords' created.");
+			
+		}
+
+		Statement stmt = conn.createStatement();
+		String query = "INSERT IGNORE INTO scramblewords (word) VALUES ('riak')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('amazon')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('aws')";
+		stmt.executeUpdate(query);
+		query = "INSERT IGNORE INTO scramblewords (word) VALUES ('dynamo')";
+		stmt.executeUpdate(query);
+
+		logger.info("Table scrablewords inserted with values");
+		System.out.println("Table scramblewords inserted with values");
+				
+		conn.close();			
+	}	
 	
 	@RequestMapping(value = "scramble", method = RequestMethod.POST)
 	public String enterWord(HttpServletRequest request, HttpSession session, Model model) {		
